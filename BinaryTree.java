902139ebdfbc8d6ac37fc4c705c918ba62bafd63
Jasper Valdez <jfvaldez@gmail.com>
2024-09-11 23:32:35 +0800

    3rd

--- BinaryTree.java
+++ BinaryTree.java
@@ -2,6 +2,13 @@ import java.util.Arrays;
 
 public class BinaryTree {
     public static void main(String[] args) {
+
+//Assignment : Binary Tree
+
+//Use the recursive method to function binarySearch
+//Updated Array values : int [] arr = { 21, 23, 51, 27, 9, 11, 18, 25, 58, 69, 90 };
+//Use the existing void to create your own function to sort the values to Ascending Order
+//Any used of Built-in Libraries is prohibited
         
         int [] arr = {21, 23, 51, 27, 9, 11, 18, 25, 58, 69, 90 };
        
@@ -9,31 +16,38 @@ public class BinaryTree {
 
         // for(int i = 0; i< arr.length; i++){
         //     System.out.println(arr[i] + " ");
-        
-        System.out.println(binarySearch(arr, 27) +"" + " is the Index Number : ");
+        int index = binarySearch(arr, 27, 0, arr.length - 1);
+        if (index != -1) {
+
+            System.out.println();
+     System.out.println("27 is at index " + index + " is the Index Number  ");
+    } else {
+        System.out.println("27 is not found in the array.");
+    }
+   
     
         System.out.println(Arrays.binarySearch(arr, 27)+ "" + " is the Built-in Index :");
 
     }
-    private static int binarySearch(int [] numbers, int numberToFind){
-        int low = 0;
-        int high = numbers.length -1;
+  private static int binarySearch(int[] numbers, int numberToFind, int low, int high) {
+        if (low > high) {
+            return -1; 
+        }
 
-        while (low <= high) {
-            int middlePosition = (low + high) /2;
-            int middleNumber = numbers[middlePosition];
+        int middlePosition = (low + high) / 2;
+        int middleNumber = numbers[middlePosition];
 
-            if(numberToFind == middleNumber){
-                return middlePosition;
-            }
-            if(numberToFind < middleNumber){
-                high = middlePosition-1;
-            }else{
-                low = middlePosition+1;
-            }
-      }
-      return -1;
+        if (numberToFind == middleNumber) {
+            return middlePosition; 
+        }
+
+        if (numberToFind < middleNumber) {
+            return binarySearch(numbers, numberToFind, low, middlePosition - 1); 
+        } else {
+            return binarySearch(numbers, numberToFind, middlePosition + 1, high); 
+        }
     }
+
     private static void ascOrder (int [] arr){
         
         // Outer loop
